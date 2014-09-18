cb1b2d8ebf8cca9c3e190ad47754b5aa049f4258
danielabdalla <danielabdalla@me4324-02.labs.sce.carleton.ca>
2014-09-18 14:42:45 -0400

    Address book print

---
+++ src/AddressBook.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+
+public class AddressBook {
+	private ArrayList<BuddyInfo> buddies;
+
+
+	public AddressBook() {
+		buddies = new ArrayList<BuddyInfo>();
+	}
+	
+	public void addBuddy(BuddyInfo buddy){
+		buddies.add(buddy);
+	}
+	
+	public void removeBuddy(BuddyInfo buddy){
+		buddies.remove(buddy);
+	}
+	
+	public static void main(String args[]){
+		System.out.println("Address book");
+	}
+}
