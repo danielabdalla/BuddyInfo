e1f59de564fd6d49e062d39d97424a0eff1702fe
danielabdalla <danielabdalla@me4324-03.labs.sce.carleton.ca>
2014-10-09 13:44:05 -0400

    Added equals method

--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -34,5 +34,19 @@ public class BuddyInfo {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
+	
+	public String toString(){
+		return name + ", " + address + ", " + phone;
+	}
+	
+	public boolean equals(Object obj){
+		if(obj instanceof BuddyInfo){
+			if(((BuddyInfo) obj).name.equals(getName()) &&
+			((BuddyInfo) obj).address.equals(getAddress()) &&
+			((BuddyInfo) obj).phone.equals(getPhone())){
+				return true;
+			}
+		}
+		return false;
+		}
 }
